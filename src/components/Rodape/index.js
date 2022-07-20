169596e370feae3c4efcb4705d988c0a42256fee
Caio Ferraresi <caio.lferraresi@gmail.com>
2022-07-20 02:23:51 -0300

    First Commit

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './style.css';
+
+export default function Rodape() {
+  return (
+    <div id='Rodape' className="Rodape">
+    </div>
+  );
+}
---
+++ src/components/Topo/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './style.css';
+
+export default function Topo() {
+  return (
+    <div id='Topo' className="Topo">
+    </div>
+  );
+}
